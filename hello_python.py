c4282104a7b7f57e92e32d1352958a95bfb7da85
MishellYerovi <mishellfriend@hotmail.com>
2025-06-24 21:53:34 -0500

    file modificated_

--- hello_python.py
+++ hello_python.py
@@ -1,4 +1,4 @@
 name= input ("¿Cuál es tu nombre?: ") #Solicitar un dato
-my_list = [1, 2, 3, 4] #Crear una lista
+my_list = [1, 2, 3, 4,5] #Crear una lista
 my_list_len= len(my_list) #obtener el número de elementos
-print("Hola,",  name,".","El número de elementos de la lista", my_list_len);
+print("Hola,",  name,"."," El número de elementos de la lista", my_list_len);
