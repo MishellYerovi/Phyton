0286304e04f7e319030c51bf36d802f0924aede5
MishellYerovi <mishellfriend@hotmail.com>
2025-06-25 00:10:06 -0500

    condicionales

---
+++ condicionales_edad.py
@@ -0,0 +1,11 @@
+
+my_age = int(input("Ingrese su edad: "))  #variable
+if my_age >=18:   #condicional
+    print ("Soy mayor de edad");
+elif my_age == 17: 
+        print ("Dentro de un año seré mayor de edad")
+elif my_age ==0:
+        print("Eres un bebé, aún no cumples tu primer año de edad")
+else:
+    
+    print ("Soy menor de edad")
